ccd41ab1540675e65fb00f715c8be20d490332ed
mayu.watanabe <m_popo.xx@i.softbank.jp>
2025-01-07 22:54:41 +0900

    課題です。

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -3,10 +3,10 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable; 
-import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam; 
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -29,7 +29,7 @@ public class CountryController {
 
     // ----- 追加:ここから -----
     // ----- 詳細画面 -----
-    @GetMapping(value = { "/detail", "/detail/{code}/" })
+    @GetMapping(value = { "/detail/{code}/" })
     public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
         // codeが指定されていたら検索結果、無ければ空のクラスを設定
         Country country = code != null ? service.getCountry(code) : new Country();
@@ -51,8 +51,12 @@ public class CountryController {
     }
 
     // ----- 削除画面 -----
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = { "/delete/{code}/" })
+    public String postCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country);
         // country/delete.htmlに画面遷移
         return "country/delete";
     }
